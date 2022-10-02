2a037a321b66aa4f87fbf42334284d0c6b138902
oshadaera68 <mindartlk010@gmail.com>
2022-10-02 22:31:13 +0530

    updated content

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const mysql = require('mysql')
 const express = require('express')
+const vehicle = require('./routes/Vehicle')
 const app = express()
 const port = 4000
+
+app.use(express.json())
+
+app.use('./vehicle', vehicle)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
